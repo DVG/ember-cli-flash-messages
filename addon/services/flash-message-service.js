eefec9925b5a15a22603fe4afff686498c3123b2
Dan de Havilland <dan@dandehavilland.com>
2015-01-05 18:55:21 +0100

    Deferring clean-up until previous run loop returns

--- addon/services/flash-message-service.js
+++ addon/services/flash-message-service.js
@@ -1,28 +1,28 @@
 import Ember from 'ember';
 import FlashMessage from 'flash-messages/models/flash';
- 
+
 export default Ember.Object.extend({
   queue:   Ember.A([]),
   content: Ember.computed.alias('queue'),
   isEmpty: Ember.computed.equal('queue.length', 0),
   timeout: 2000,
-  
+
   success: function(msg){
     this._add(msg, 'success');
   },
-  
+
   info: function(msg){
     this._add(msg, 'info');
   },
-  
+
   warning: function(msg){
     this._add(msg, 'warning');
   },
-  
+
   danger: function(msg){
     this._add(msg, 'danger');
   },
-  
+
   // private
   _add: function(msg, type){
     var flashes, flash;
@@ -30,28 +30,30 @@ export default Ember.Object.extend({
     flash   = this._newFlashMessage(msg, type);
     flashes.pushObject(flash);
   },
-  
+
   _newFlashMessage: function(msg, type){
     var timeout;
     Ember.assert('Must pass a valid flash message', msg);
     type    = (typeof type === 'undefined') ? 'info' : type;
     timeout = Ember.get(this, 'timeout');
-    
+
     return FlashMessage.create({
       type:    type,
       message: msg,
       timeout: timeout
     });
   },
-  
+
   _queueDidChange: Ember.observer('queue.@each.isDestroyed', function(){
     var flashes, destroyed, timeout;
     flashes =  Ember.get(this, 'queue');
     timeout =  Ember.get(this, 'timeout');
-    
-    Ember.run.later(this, function(){
-      destroyed = flashes.filterBy('isDestroyed', true);
-      return flashes.removeObjects(destroyed);
+
+    Ember.run.later(function(){
+      Ember.run.next(function() {
+        destroyed = flashes.filterBy('isDestroyed', true);
+        return flashes.removeObjects(destroyed);
+      });
     }, timeout);
   })
 });
